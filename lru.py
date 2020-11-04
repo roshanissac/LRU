3350bdda99bd28ec7ff757a039f2ac3cd81125e1
roshanissac <roshan.issac@dcmail.ca>
2020-11-04 18:57:14 -0500

    Created class and initialized the max size of the cache

---
+++ lru.py
@@ -0,0 +1,13 @@
+from collections import OrderedDict
+
+class lruCache:
+    def __init__(self, size: int):
+        self.cache = OrderedDict()
+        self.size = size
+
+
+
+
+#Initializing cache with max size
+cache=lruCache(3)
+print(cache.size)
